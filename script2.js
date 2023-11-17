a19c58731e593db7a8d9eacc1f11528ab00ba2e4
dxgabalt <71161987+dxgabalt@users.noreply.github.com>
2023-11-16 18:13:01 -0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const startCallButton = document.getElementById('startCall');
+    const videoContainer = document.getElementById('videoContainer');
+
+    startCallButton.addEventListener('click', () => {
+        // Lógica para iniciar la videollamada con WebRTC
+        // Puedes utilizar bibliotecas como SimpleWebRTC o implementar WebRTC directamente aquí
+    });
+});
